063388d004b894f4f45632f052ac10e24f2c9e83
Mohib alfonick <mohib.alfonick@gmail.com>
2026-05-30 22:16:14 +0500

    homepage required changes

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -8,10 +8,8 @@ function clamp(value: number, min: number, max: number) {
 }
 
 export function SiteHeader() {
-
   return (
-    <header
-    className="absolute left-0 top-0 z-50 w-full px-4 bg-transparent pt-5 transition-all duration-300 sm:px-6 sm:pt-7 lg:px-14 lg:pt-9 xl:px-14.5">
+    <header className="absolute left-0 top-0 z-50 w-full px-4 bg-transparent pt-5 transition-all duration-300 sm:px-6 sm:pt-7 lg:px-14 lg:pt-9 xl:px-14.5">
       <div className="mx-auto flex max-w-420 items-center justify-between">
         {/* Logo */}
         <Link
@@ -32,7 +30,6 @@ export function SiteHeader() {
           />
         </Link>
 
-<<<<<<< HEAD
         {/* Book Button */}
         {/* <Link
   href="#contact"
@@ -53,18 +50,14 @@ export function SiteHeader() {
   </span>
 </Link> */}
 
-
         <Link
-          href="#" className="headerBtn group border rounded-full pr-4 pl-15 py-3 hover:pr-15! hover:pl-4! text-white transition-all duration-300 sm:text-[18px] text-[16px]">
-         
-=======
-        <Link href="#" className="headerBtn group border rounded-full md:py-3 sm:py-4 py-3! sm:pr-5 pr-3 sm:pl-15 pl-12 hover:pr-15 hover:pl-5 active:pl-5 sm:active:pr-15 active:pr-12 text-white transition-all duration-300">
+          href="#"
+          className="headerBtn group border rounded-full md:py-3 sm:py-4 py-3! sm:pr-5 pr-3 sm:pl-15 pl-12 hover:pr-15 hover:pl-5 active:pl-5 sm:active:pr-15 active:pr-12 text-white transition-all duration-300"
+        >
           <span className="inline-block transition-all duration-300 lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]">
->>>>>>> 583e4a47c7dad5e10658891ea74f4b98c5bcd0b1
             Book A Demo
- 
+          </span>
         </Link>
-
       </div>
     </header>
   );
